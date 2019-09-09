2bc685f0fcac38fdb3363abc06063930ffe3b4ac
Ahmad Mahmoud <ams.eng@hotmail.com>
2019-09-09 22:35:31 +0200

    Small change in BaseRestClient class.

--- AndroidBase/androidBase/src/main/java/ams/android_base/baseClasses/networking/BaseRestClient.java
+++ AndroidBase/androidBase/src/main/java/ams/android_base/baseClasses/networking/BaseRestClient.java
@@ -1,9 +1,42 @@
 package ams.android_base.baseClasses.networking;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 public abstract class BaseRestClient<APIs> extends BaseRetrofitClient {
 
+    private Class<APIs> apIsClassType;
+
+
+    public BaseRestClient() {
+        this.apIsClassType = (Class<APIs>) getGenericClassType(0);
+    }
+
     public APIs getAPIs() {
-        Class<APIs> clazz = new GenericClass<APIs>().getRawType();
-        return retrofit.create(clazz);
+        return retrofit.create(apIsClassType);
+    }
+
+//    public APIs getAPIs() {
+//        Class<APIs> clazz = new GenericClass<APIs>().getRawType();
+//        return retrofit.create(clazz);
+//    }
+
+    /**
+     * Returns a {@link Type} object to identify generic types
+     * @return type
+     */
+    private Type getGenericClassType(int index) {
+
+        Type type = getClass().getGenericSuperclass();
+
+        while (!(type instanceof ParameterizedType)) {
+            if (type instanceof ParameterizedType) {
+                type = ((Class<?>) ((ParameterizedType) type).getRawType()).getGenericSuperclass();
+            } else {
+                type = ((Class<?>) type).getGenericSuperclass();
+            }
+        }
+
+        return ((ParameterizedType) type).getActualTypeArguments()[index];
     }
 }
