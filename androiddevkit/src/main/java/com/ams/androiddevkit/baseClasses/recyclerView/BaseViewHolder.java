50501d04f9b3ce15691755c9720d51ac9cb11098
Ahmad Mahmoud <ams.eng@hotmail.com>
2019-09-12 00:50:04 +0200

    Updates

--- androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/recyclerView/BaseViewHolder.java
+++ androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/recyclerView/BaseViewHolder.java
@@ -1,12 +1,21 @@
 package com.ams.androiddevkit.baseClasses.recyclerView;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.ButterKnife;
 
 public abstract class BaseViewHolder<T, RVListener> extends RecyclerView.ViewHolder {
+
+    public BaseViewHolder(ViewGroup parent, @LayoutRes int resLayout) {
+        super(LayoutInflater.from(parent.getContext()).inflate(resLayout, parent, false));
+        bindViews(itemView);
+    }
 
     public BaseViewHolder(View view) {
         super(view);
@@ -24,4 +33,8 @@ public abstract class BaseViewHolder<T, RVListener> extends RecyclerView.ViewHol
      */
     @SuppressWarnings("WeakerAccess")
     protected abstract void onBind(T item, @Nullable RVListener listener);
+
+    protected Context getContext() {
+        return itemView.getContext();
+    }
 }
