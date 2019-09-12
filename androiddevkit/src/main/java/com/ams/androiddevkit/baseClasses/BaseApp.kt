1205edd87e329ff96675f1ec11d2ea7427d7abdd
Ahmad Mahmoud <ams.eng@hotmail.com>
2019-09-12 17:59:16 +0200

    Updates

--- androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/BaseApp.kt
+++ androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/BaseApp.kt
@@ -1,12 +1,14 @@
 package com.ams.androiddevkit.baseClasses
 
 import android.app.Application
+import android.content.Context
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.Koin
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 
-abstract class BaseApp: Application() {
+abstract class BaseApp<App: Application>: Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -18,10 +20,13 @@ abstract class BaseApp: Application() {
     protected fun injectKoin() {
         // Start koin with the modules list
         startKoin {
-            androidContext(this@BaseApp)
+            androidLogger()
+            androidContext(getAndroidContext())
             modules(getKoinModules())
         }
     }
+
+    protected abstract fun getAndroidContext(): Context;
 
     protected abstract fun getKoinModules(): List<Module>
 }
