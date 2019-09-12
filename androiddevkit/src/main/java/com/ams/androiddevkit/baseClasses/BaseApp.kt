5bcf53d5df17bce4051b4e0849c597774bb659e2
Ahmad Mahmoud <ams.eng@hotmail.com>
2019-09-12 18:32:29 +0200

    Update

--- androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/BaseApp.kt
+++ androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/BaseApp.kt
@@ -1,32 +1,22 @@
 package com.ams.androiddevkit.baseClasses
 
 import android.app.Application
-import android.content.Context
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.Koin
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 
 abstract class BaseApp: Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        //
-        injectKoin()
-    }
 
     @Suppress("MemberVisibilityCanBePrivate")
     protected fun injectKoin() {
         // Start koin with the modules list
         startKoin {
             androidLogger()
-            androidContext(getAndroidContext())
+            androidContext(this@BaseApp)
             modules(getKoinModules())
         }
     }
-
-    protected abstract fun getAndroidContext(): Context;
 
     protected abstract fun getKoinModules(): List<Module>
 }
