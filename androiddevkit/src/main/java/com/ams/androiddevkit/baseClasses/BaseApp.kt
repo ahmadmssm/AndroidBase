00d93b26823523b910ec094cb76a58a0fb488163
Ahmad Mahmoud <ams.eng@hotmail.com>
2019-09-12 18:43:36 +0200

    Update

--- androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/BaseApp.kt
+++ androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/BaseApp.kt
@@ -1,6 +1,7 @@
 package com.ams.androiddevkit.baseClasses
 
 import android.app.Application
+import android.content.Context
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -13,10 +14,12 @@ abstract class BaseApp: Application() {
         // Start koin with the modules list
         startKoin {
             androidLogger()
-            androidContext(this@BaseApp)
+            androidContext(getAppContext())
             modules(getKoinModules())
         }
     }
+
+    abstract fun getAppContext(): Context
 
     protected abstract fun getKoinModules(): List<Module>
 }
