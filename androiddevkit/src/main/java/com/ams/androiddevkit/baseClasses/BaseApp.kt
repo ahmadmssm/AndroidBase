3f94fdf5da6936f79091625615993809a5276048
Ahmad Mahmoud <ams.eng@hotmail.com>
2019-11-09 17:49:35 +0200

    Updates

--- androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/BaseApp.kt
+++ androiddevkit/src/main/java/com/ams/androiddevkit/baseClasses/BaseApp.kt
@@ -2,24 +2,51 @@ package com.ams.androiddevkit.baseClasses
 
 import android.app.Application
 import android.content.Context
+import android.os.StrictMode
+import androidx.appcompat.app.AppCompatActivity
+import com.ams.androiddevkit.BuildConfig
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 
 abstract class BaseApp: Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            val threadPolices = StrictMode.ThreadPolicy.Builder()
+                 // or .detectAll() for all detectable problems
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build()
+            val vmPolices = StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .build()
+            //
+            StrictMode.setThreadPolicy(threadPolices)
+            StrictMode.setVmPolicy(vmPolices)
+        }
+        injectKoin()
+    }
+
     @Suppress("MemberVisibilityCanBePrivate")
-    protected open fun injectKoin() {
-        // Start koin with the modules list
+    protected fun injectKoin() {
         startKoin {
-            androidLogger()
-            androidContext(getAppContext())
+            logger(getKoinLoggingLevel())
+            androidContext(this@BaseApp)
             modules(getKoinModules())
         }
     }
 
     abstract fun getAppContext(): Context
+
+    abstract fun getCurrentActivity(): AppCompatActivity?
+
+    protected abstract fun getKoinLoggingLevel(): org.koin.core.logger.Logger
 
     protected abstract fun getKoinModules(): List<Module>
 }
