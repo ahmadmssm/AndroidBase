d143e64b83c8a1b24d8db35fd469916125583d50
Ahmad Mahmoud <ams.eng@hotmail.com>
2020-12-04 16:02:52 +0200

    Small fix

--- androiddevkit/src/main/java/com/ams/androiddevkit/utils/extensions/FragmentExtensions.kt
+++ androiddevkit/src/main/java/com/ams/androiddevkit/utils/extensions/FragmentExtensions.kt
@@ -1,9 +1,9 @@
 package com.ams.androiddevkit.utils.extensions
 
-import com.ams.androiddevkit.utils.viewArguments.fragmentArgs.BaseFragmentArgs
+import com.ams.androiddevkit.utils.viewArguments.BaseViewArgs
 import com.ams.androiddevkit.utils.viewArguments.fragmentArgs.FragmentArgs
 import com.ams.androiddevkit.utils.viewArguments.fragmentArgs.NullableFragmentArgs
 
-inline fun <reified A: BaseFragmentArgs> fragmentArgs() = FragmentArgs(A::class)
+inline fun <reified A: BaseViewArgs> fragmentArgs() = FragmentArgs(A::class)
 
-inline fun <reified A: BaseFragmentArgs> nullableFragmentArgs() = NullableFragmentArgs(A::class)
+inline fun <reified A: BaseViewArgs> nullableFragmentArgs() = NullableFragmentArgs(A::class)
